fd773cd4a6a4fda084eaa4cfb15c7637cced8193
git <>
2017-06-07 23:44:31 +0800

    liuwenwen

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -17,9 +17,9 @@ function MainLayout({ children, location }) {
           defaultSelectedKeys={['2']}
           style={{ lineHeight: '64px' }}
         >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">首页</Menu.Item>
+          <Menu.Item key="2">更换皮肤</Menu.Item>
+          <Menu.Item key="3">切换视图</Menu.Item>
         </Menu>
       </Header>
       <Layout>
